dde944697efddd43d53bf7865a554a64240b4730
Valerii <fazlyvalera@gmail.com>
2016-05-11 00:01:43 +0300

    update styles

--- js/main.js
+++ js/main.js
@@ -15,6 +15,25 @@ $(function(){
         });
         $('.modal-auth input').on('blur', app.checkAuthFormInputs);
         app.accordionHeadingClass();
+
+        if($(document).width() > 640){
+          $('.vacancy-meta-wrapper').affix({
+            offset: {
+              top: $('.vacancy-meta-wrapper').offset().top + 67 - $('header').height(),
+              bottom: 1500
+            }
+          }).on('affix.bs.affix', function(){
+            $(this).width($(this).width());
+          }).on('affix-top.bs.affix', function(){
+            $(this).removeAttr('style');
+          });
+        }
+
+        $('header').on('affixed.bs.affix', function(){
+          $('body').css('padding-top', $('header').height());
+        }).on('affix-top.bs.affix', function(){
+          $('body').css('padding-top', 0);
+        });
     },
     checkboxed: function(el){
         $(el).parent().removeClass('checked');
